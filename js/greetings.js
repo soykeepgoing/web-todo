a73e9cb8e274cae1fc7a8b40250fa2ef41b17881
soykeepgoing <soykeepgoing@gmail.com>
2025-07-20 21:48:04 +0900

    add: 새로고침 & 페이드인아웃 기능

--- js/greetings.js
+++ js/greetings.js
@@ -1,5 +1,5 @@
 const USERNAME_KEY = "userName";
-const HIDDEN_CLASSNAME = "hidden";
+const FADE_IN_CLASSNAME = "fade-in";
 const FADE_OUT_CLASSNAME = "fade-out";
 
 const welcomeContainer = document.querySelector("#welcome-container");
@@ -12,15 +12,16 @@ function onNameSubmit(event){
     event.preventDefault(); // 기본 동작 방지 
     const userName = welcomeInput.value;
     localStorage.setItem(USERNAME_KEY, userName); // DB에 저장
-    welcomeContainer.classList.add(FADE_OUT_CLASSNAME); // 페이드아웃 클래스 적용
+    welcomeContainer.classList.remove(FADE_IN_CLASSNAME); // 등장 효과 삭제 
+    welcomeContainer.classList.add(FADE_OUT_CLASSNAME); 
 }
 
 if (savedUsername === null){
     // 저장된 사용자 이름이 없는 경우 => 시작 페이지 
-    welcomeContainer.classList.remove(HIDDEN_CLASSNAME)
-    welcomeForm.addEventListener("submit", onNameSubmit);
+    welcomeContainer.classList.add(FADE_IN_CLASSNAME); // 페이드인으로 등장
+    welcomeForm.addEventListener("submit", onNameSubmit); 
 
 } else{
     // 기본 페이지 
-    
+    welcomeContainer.classList.add(FADE_OUT_CLASSNAME); 
 }
---
+++ js/redo.js
@@ -0,0 +1,4 @@
+localStorage.clear();
+
+// const greeting = document.querySelector("#welcome-container");
+// greeting.classList.add('fade-in');
