24c7042f3b34aaed8eed59e70925642e126da3f9
soykeepgoing <soykeepgoing@gmail.com>
2025-07-20 15:20:58 +0900

    fix : 날씨 기능 html 오탈자

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,7 @@
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
-    console.log(WEATHER_API_KEY);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
-    console.log(url);
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
