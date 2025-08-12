0939f066216c7cee0f7e62b40f351b21d3eb219a
soykeepgoing <soykeepgoing@gmail.com>
2025-08-12 21:35:07 +0900

    fix: submit 기능 및 스타일 수정

--- js/todo.js
+++ js/todo.js
@@ -3,29 +3,34 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 const TODOS_KEY = "todos";
 
-function saveToDos(){
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
-}
+let toDos = []; 
 
 function paintToDo(newToDo){
     const li = document.createElement('li');
     const span = document.createElement('span');
     const button = document.createElement('button');
     li.id = newToDo.id;
-    span.innerText = newToDo.text;
+    span.innerText = newToDo; 
     button.innerText = '✅ Done';
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
 }
 
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
+}
+
 function submitToDo(event){
     event.preventDefault();
-    newToDo = {
-        text: toDoInput.value,
+    const newToDoInput = toDoInput.value;
+    const newToDoObj = {
+        text: newToDoInput,
         id: Date.now()
     }
-    paintToDo(newToDo);
+    toDos.push(newToDoObj);
+    paintToDo(newToDoInput);
+    saveToDos(newToDoObj);
     toDoInput.value = '';
 }
 
